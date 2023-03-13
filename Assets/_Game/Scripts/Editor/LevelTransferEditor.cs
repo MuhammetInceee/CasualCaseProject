900dea953d86a2a1a84bfefca50dd839c3c0b6f4
Muhammet İNCE <83353864+MuhammetInceee@users.noreply.github.com>
2023-03-13 03:42:51 +0300

    Code Refactoring

--- Assets/_Game/Scripts/Editor/LevelTransferEditor.cs
+++ Assets/_Game/Scripts/Editor/LevelTransferEditor.cs
@@ -4,7 +4,6 @@ using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// Unity Editor Window'da bir seçim menüsü oluşturur ve seçilen Gameobject'in belirli bir sahneye aktarılmasına olanak sağlar
 public class LevelTransferEditor : EditorWindow {
     private GameObject selectedObject;
     private string[] sceneNames;
@@ -16,13 +15,10 @@ public class LevelTransferEditor : EditorWindow {
     }
 
     private void OnGUI() {
-        // Seçilen Gameobject'i al
         selectedObject = Selection.activeGameObject;
 
-        // Mevcut sahnenin adını al
         string currentScene = SceneManager.GetActiveScene().name;
 
-        // Sahne isimlerini al
         int sceneCount = SceneManager.sceneCountInBuildSettings;
         sceneNames = new string[sceneCount];
         for (int i = 0; i < sceneCount; i++) {
@@ -30,17 +26,13 @@ public class LevelTransferEditor : EditorWindow {
             sceneNames[i] = System.IO.Path.GetFileNameWithoutExtension(scenePath);
         }
 
-        // Seçim menüsünü oluştur
         GUILayout.Label("Selected Object: " + (selectedObject != null ? selectedObject.name : "None"));
         GUILayout.Label("Current Scene: " + currentScene);
         selectedSceneIndex = EditorGUILayout.Popup("Transfer Object to Scene:", selectedSceneIndex, sceneNames);
 
-        // Objenin sahneye taşınması için düğme
         if (selectedObject != null && GUILayout.Button("Transfer Object")) {
-            // Seçilen sahne adını al
             string selectedScene = sceneNames[selectedSceneIndex];
 
-            // Belirtilen sahne var mı diye kontrol et
             bool sceneExists = false;
             for (int i = 0; i < sceneCount; i++) {
                 string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
@@ -50,7 +42,6 @@ public class LevelTransferEditor : EditorWindow {
                 }
             }
 
-            // Eğer belirtilen sahne varsa, objeyi o sahneye taşı
             if (sceneExists)
             {
                 CreatePreafab();
@@ -65,22 +56,17 @@ public class LevelTransferEditor : EditorWindow {
 
     private static void TransferAnotherScene(string selectedScene)
     {
-        // Kaydedilen prefabı farklı bir sahneye aktar
         string prefabPath = "Assets/Resources/Levels/" + Selection.activeGameObject.name + ".prefab";
         GameObject prefab = AssetDatabase.LoadAssetAtPath(prefabPath, typeof(GameObject)) as GameObject;
 
         if (prefab != null)
         {
-            // Farklı bir sahne aç
             EditorSceneManager.OpenScene($"Assets/Scenes/{selectedScene}.unity", OpenSceneMode.Single);
 
-            // Prefab içeriğini yükle
             GameObject instance = PrefabUtility.LoadPrefabContents(prefabPath);
 
-            // Prefabı sahneye ekle
             PrefabUtility.InstantiatePrefab(prefab);
 
-            // Prefab içeriğini kaydet ve yükleme kaynaklarını temizle
             PrefabUtility.SaveAsPrefabAsset(instance, prefabPath);
             PrefabUtility.UnloadPrefabContents(instance);
             
